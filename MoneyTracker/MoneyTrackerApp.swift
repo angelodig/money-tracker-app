0e5e6ce7fde504fa2671ad3515216dbd2c1c95f8
angelodig <anreg@hotmail.it>
2020-11-18 10:09:26 +0100

    Initial Commit

---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Angelo Di Gianfilippo on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
